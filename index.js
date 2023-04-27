14699c32a74d34349189452b9337792abbdf6595
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-04-27 11:48:20 +0900

    0.0.3 feat: migration server code

--- index.js
+++ index.js
@@ -1,19 +1,65 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3002;
+
+app.use(bodyParser.json());
 
 // add folder calcul_project/builds
 app.use(express.static('calcul_project/builds'));
 // app.use()
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World!');
+const root_data = {
+    num: 1
+};
+
+// app.get(['/', '/index.html'], function (req, res) {
+
+//     // res.sendFile(__dirname + '/index.html');
+//     res.sendFile(__dirname + '/builds/index.html');
+//     // res.send('Hello Elastic Beanstalk');
 // });
 
-// app.get('/calc', (req, res) => {
-//     res.sendFile(__dirname + '/calcul_project/builds/calculator.html');
-//     // res.sendFile(__dirname + '/calculator.html');
-// });
+// 01 랜덤 숫자 요청
+app.get('/api/req/num', function (req, res) {
+    // res.send(1);    
+    // const data = {
+    //     num: 1
+    // }
+    // res.setHeader('Content-Type', 'application/json');
+
+    // console.log(data);
+    // res.json(JSON.stringify(data));
+
+    res.sendStatus(200);
+});
+
+// 02 계산기 데이터 저장
+app.post('/api/data/save', function (req, res) {
+    // res.send('Hello Elastic Beanstalk');
+    const data = req.body;
+
+    // 데이터 저장하기
+    console.log(data);
+    root_data.num = data.value;
+
+    res.sendStatus(200);
+});
+
+// 03 저장된 계산기 데이터 불러오기
+app.get('/api/data/req', function (req, res) {
+
+    // 저장된 데이터가 1이다.
+    const data = {
+        value: root_data.num
+    }
+    res.setHeader('Content-Type', 'application/json');
+
+    console.log(data);
+    res.json(JSON.stringify(data));
+});
+
+
 
 app.listen(port, () => { 
     console.log(`Example app listening at http://localhost:${port}`);
